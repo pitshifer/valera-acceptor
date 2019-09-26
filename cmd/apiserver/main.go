8c468722f9767275b86e23563e922d5941b2283b
anton.kvasov <anton.kvasov@fxclub.com>
2019-09-26 16:57:59 +0700

    need commit

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/pitshifer/valera-acceptor/internal/apiserver"
+)
+
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "c", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := apiserver.New(config)
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pitshifer/valera-acceptor
+
+go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/sirupsen/logrus v1.4.2
+)
